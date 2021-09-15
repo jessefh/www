fac9120f1a7373177483ea4a3bbe7cb10cde4f6d
Jesse Haenen <jessehaenen@pm.me>
2021-09-15 22:29:41 +0200

    Add function to get current book from Goodreads

---
+++ api/current_book.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from http.server import BaseHTTPRequestHandler
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+
+        URL = "https://www.goodreads.com/user/show/65474722-jesse"
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        currently_reading = soup.find(id="currentlyReadingReviews")
+        book_title = currently_reading.find_all("a", class_="bookTitle")[0]
+        book_author = currently_reading.find_all("a", class_="authorName")[0]
+
+        self.wfile.write(f"title:{book_title}, author:{book_author}".encode())
+        return
+
