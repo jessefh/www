f6c3b868d2de84f4929c900d86bca9f4d96f73f0
Jesse Haenen <jessehaenen@pm.me>
2021-09-16 11:40:45 +0200

    Send header after retrieving data

--- api/book.py
+++ api/book.py
@@ -7,21 +7,22 @@ from bs4 import BeautifulSoup
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-
         URL = "https://www.goodreads.com/user/show/65474722-jesse"
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, "html.parser")
 
         currently_reading = soup.find(id="currentlyReadingReviews")
-        data = {
+        current_book = {
             "book": currently_reading.find_all("a", class_="bookTitle")[0],
             "author": currently_reading.find_all("a", class_="authorName")[0],
         }
-        json_string = json.dumps(data)
+        output_data = json.dumps(current_book, ensure_ascii=False).encode("utf-8")
 
-        self.wfile.write(bytes(json.dumps(json_string, ensure_ascii=False), "utf-8"))
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Content-Length", str(len(output_data)))
+        self.end_headers()
+
+        self.wfile.write(output_data)
         return
 
